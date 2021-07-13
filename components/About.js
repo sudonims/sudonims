89bfdcb0f5b97d3edfd00e2d0314998c0729207e
Nimish Shah <nimishshah2000@gmail.com>
2021-07-13 18:29:48 +0530

    about done

--- components/About.js
+++ components/About.js
@@ -13,13 +13,24 @@ export default function About() {
         marginLeft="10vw"
         marginRight="10vw"
       >
-        <Text display={{ base: 'none', md: 'block' }} fontSize="lg">
-          Hi, my name is
+        <Text
+          display={{ base: 'none', md: 'block' }}
+          fontSize="xl"
+          marginBottom="1"
+          letterSpacing="wide"
+        >
+          &nbsp;Hi, my name is
         </Text>
         <Text display={{ base: 'block', md: 'none' }} fontSize="md">
           Hi, my name is
         </Text>
-        <Heading display={{ base: 'none', md: 'block' }} size="4xl" as="h1">
+        <Heading
+          display={{ base: 'none', md: 'block' }}
+          size="4xl"
+          as="h1"
+          marginBottom="1"
+          letterSpacing="wide"
+        >
           Nimish Shah.
         </Heading>
         <Heading display={{ base: 'block', md: 'none' }} size="2xl" as="h1">
@@ -30,6 +41,7 @@ export default function About() {
           color="gray.400"
           size="4xl"
           as="h2"
+          letterSpacing="wide"
         >
           I develop things for the web
         </Heading>
@@ -41,6 +53,23 @@ export default function About() {
         >
           I develop things for the web
         </Heading>
+        <div style={{ maxWidth: '40%', marginTop: 20, marginBottom: 20 }}>
+          <Text display={{ base: 'none', md: 'block' }} letterSpacing="wide">
+            I am a <b>Software Engineer</b> good at developing web interfaces.
+            Always wanting to try out new things, I sometimes accidently also
+            develop small useful routines.
+          </Text>
+          <Text display={{ base: 'block', md: 'none' }}>
+            I am a <b>Software Engineer</b> good at developing web interfaces.
+            Always wanting to try out new things, I sometimes accidently also
+            develop small useful routines.
+          </Text>
+        </div>
+        <Text color="#64ffda">
+          <a className="awesome" href="mailto:nimishshah2000@gmail.com">
+            Let's make something awesome !!
+          </a>
+        </Text>
       </Container>
     </section>
   );
