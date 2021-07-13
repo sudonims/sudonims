a7be75b5c4b7ccffe315a08d4842851fd424a066
sudonims <nimishshah2000@gmail.com>
2021-07-13 17:04:47 +0000

    staged deploy

--- _next/static/PyeusgD0NCKminpdGQVv5/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/355-e9d4745b007435bfe98d.js","static/chunks/pages/index-cb1dd326544d802a3df1.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
