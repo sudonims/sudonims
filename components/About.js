18d508bba38a2ddb57513e604d321a14dd38453e
Nimish Shah <nimishshah2000@gmail.com>
2021-07-15 00:42:32 +0530

    profile changes

--- components/About.js
+++ components/About.js
@@ -1,21 +1,39 @@
-import { Heading, Flex } from '@chakra-ui/react';
+import { Heading, Flex, Text, Box } from '@chakra-ui/react';
 
 export default function About() {
+  const mouseEnter = (e) => {
+    console.log(e);
+  };
   return (
-    <section className="section">
+    <section onMouseEnter={mouseEnter} id="about" className="section">
       <Flex justifyContent="center" flexDirection="column" height="100%">
-        <Flex margin="0px 100px" direction="row" align="flex-start">
+        <Flex margin="20px 15%" direction="row" align="flex-start">
           <Heading letterSpacing="wider" fontSize="7xl">
             Me & Myself
           </Heading>
         </Flex>
-        <Flex>
-          <div className="image">
-            <div className="image-grey-right"></div>
-            <div className="image-grey-middle"></div>
-            <div className="image-grey-left"></div>
-          </div>
-          {/* <img src="/static/image/profile.jpg" /> */}
+        <Flex margin="20px 10%" justifyContent="flex-start">
+          <Box height="1px" width="30%" backgroundColor="aliceblue"></Box>
+        </Flex>
+        <Flex margin="20px 15%" direction="row" align="flex-start">
+          <Flex direction="column" maxWidth="40%" alignContent="space-around">
+            <Text fontSize="lg" letterSpacing="wide" margin="10px">
+              I am a Full Stack Web Developer located in India. I enjoy creating
+              things for the web. I have a passion for trying out new things
+              that I find interesting. Started Web Development for a college
+              project, I grew interest in it and haven't looked back since.
+            </Text>
+            <Text fontSize="lg" letterSpacing="wide" margin="10px">
+              Problem solver, well organized with high attention to details. I
+              am a tech enthusiast and like to understand core things about
+              tech.
+            </Text>
+          </Flex>
+          <Flex margin="-10% 0px 0px 15%">
+            <div className="image">
+              <div className="image-grey-right"></div>
+            </div>
+          </Flex>
         </Flex>
       </Flex>
     </section>
