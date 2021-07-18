21b8e9337addc4e257c67a459367ceb462b6e271
sudonims <nimishshah2000@gmail.com>
2021-07-18 18:14:28 +0000

    staged deploy

---
+++ _next/static/DDVu59WWC_--qYqUdszke/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/355-e9d4745b007435bfe98d.js","static/chunks/pages/index-87dbc97347fd1bc93bc1.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
