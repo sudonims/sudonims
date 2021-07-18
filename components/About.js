f7353c3a49ff76ab1020ab7f6482ed2f39ff486d
Nimish Shah <nimishshah2000@gmail.com>
2021-07-18 23:40:56 +0530

    remove redundancy

--- components/About.js
+++ components/About.js
@@ -7,29 +7,59 @@ export default function About() {
   return (
     <section onMouseEnter={mouseEnter} id="about" className="section">
       <Flex justifyContent="center" flexDirection="column" height="100%">
-        <Flex margin="20px 15%" direction="row" align="flex-start">
-          <Heading letterSpacing="wider" fontSize="7xl">
+        <Flex margin={{ md: '20px 15%' }} direction="row" align="flex-start">
+          <Heading
+            letterSpacing="wider"
+            width={{ base: '100%', md: null }}
+            textAlign={{}}
+            fontSize={{ base: '4xl', md: '7xl' }}
+          >
             Me & Myself
           </Heading>
         </Flex>
-        <Flex margin="20px 10%" justifyContent="flex-start">
+        <Flex
+          display={{ base: 'none', md: 'flex' }}
+          margin={{ md: '20px 10%' }}
+          justifyContent="flex-start"
+        >
           <Box height="1px" width="30%" backgroundColor="aliceblue"></Box>
         </Flex>
-        <Flex margin="20px 15%" direction="row" align="flex-start">
-          <Flex direction="column" maxWidth="40%" alignContent="space-around">
-            <Text fontSize="lg" letterSpacing="wide" margin="10px">
+        <Flex
+          width={{ base: '100%' }}
+          margin={{ md: '20px 15%' }}
+          direction={{ base: 'column', md: 'row' }}
+          align="flex-start"
+        >
+          <Flex
+            direction="column"
+            maxWidth={{ md: '40%' }}
+            alignContent="space-around"
+          >
+            <Text
+              fontSize={{ base: 'md', md: 'lg' }}
+              letterSpacing="wide"
+              margin="10px"
+            >
               I am a Full Stack Web Developer located in India. I enjoy creating
               things for the web. I have a passion for trying out new things
               that I find interesting. Started Web Development for a college
               project, I grew interest in it and haven't looked back since.
             </Text>
-            <Text fontSize="lg" letterSpacing="wide" margin="10px">
+            <Text
+              fontSize={{ base: 'md', md: 'lg' }}
+              letterSpacing="wide"
+              margin="10px"
+            >
               Problem solver, well organized with high attention to details. I
               am a tech enthusiast and like to understand core things about
               tech.
             </Text>
           </Flex>
-          <Flex margin="-10% 0px 0px 15%">
+          <Flex
+            alignContent={{ base: 'center' }}
+            margin={{ md: '-10% 0px 0px 15%' }}
+            display={{ base: 'none', md: 'flex' }}
+          >
             <div className="image">
               <div className="image-grey-right"></div>
             </div>
--- components/Home.js
+++ components/Home.js
@@ -1,4 +1,4 @@
-import { Heading, Container, Text } from '@chakra-ui/react';
+import { Heading, Container, Text, Box } from '@chakra-ui/react';
 
 export default function Home() {
   return (
@@ -10,61 +10,40 @@ export default function Home() {
         height="100%"
         width="100%"
         maxWidth="100%"
-        marginLeft="10vw"
-        marginRight="10vw"
+        marginLeft={{ md: '10vw' }}
+        marginRight={{ md: '10vw' }}
       >
         <Text
-          display={{ base: 'none', md: 'block' }}
-          fontSize="xl"
+          fontSize={{ base: 'md', md: 'xl' }}
           marginBottom="1"
           letterSpacing="wide"
         >
           &nbsp;Hi, my name is
         </Text>
-        <Text display={{ base: 'block', md: 'none' }} fontSize="md">
-          Hi, my name is
-        </Text>
         <Heading
-          display={{ base: 'none', md: 'block' }}
-          fontSize="8xl"
+          fontSize={{ base: '4xl', md: '8xl' }}
           as="h1"
           marginBottom="1"
           letterSpacing="wide"
         >
           Nimish Shah.
         </Heading>
-        <Heading display={{ base: 'block', md: 'none' }} size="2xl" as="h1">
-          Nimish Shah.
-        </Heading>
         <Heading
-          display={{ base: 'none', md: 'block' }}
           color="gray.400"
+          fontSize={{ base: '2xl', md: '7xl' }}
           size="4xl"
           as="h2"
           letterSpacing="wide"
         >
           I develop things for the web
         </Heading>
-        <Heading
-          display={{ base: 'block', md: 'none' }}
-          color="gray.400"
-          size="2xl"
-          as="h2"
-        >
-          I develop things for the web
-        </Heading>
-        <div style={{ maxWidth: '40%', marginTop: 20, marginBottom: 20 }}>
-          <Text display={{ base: 'none', md: 'block' }} letterSpacing="wide">
+        <Box margin="20px 0px" width={{ base: '100%', md: '40%', lg: '40%' }}>
+          <Text noOfLines={[5, 4, 4]} letterSpacing="wider">
             I am a <b>Software Engineer</b> good at developing web interfaces.
             Always wanting to try out new things, I sometimes accidently also
             develop small useful routines.
           </Text>
-          <Text display={{ base: 'block', md: 'none' }}>
-            I am a <b>Software Engineer</b> good at developing web interfaces.
-            Always wanting to try out new things, I sometimes accidently also
-            develop small useful routines.
-          </Text>
-        </div>
+        </Box>
         <Text color="#64ffda">
           <a className="awesome" href="mailto:nimishshah2000@gmail.com">
             Let&#39;s make something awesome !!
