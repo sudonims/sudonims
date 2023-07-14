59f6a4b5500dd27a241cf768117155548511093b
Nimish Shah <nimishshah2000@gmail.com>
2023-07-14 23:22:38 +0530

    test

--- pages/test.js
+++ pages/test.js
@@ -6,7 +6,7 @@ export default function Check() {
         width="600"
         height="450"
         style={{ border: 0 }}
-        allowfullscreen=""
+        allowFullScreen=""
         loading="lazy"
         referrerpolicy="no-referrer-when-downgrade"
       ></iframe>
