018d14478a01c64ef1f830a90fdc330a008068b4
Nimish Shah <nimishshah2000@gmail.com>
2024-12-10 21:35:48 +0530

    resume fix

--- pages/resume.js
+++ pages/resume.js
@@ -1,22 +1,21 @@
-import { useEffect } from 'react';
+import { Container } from '@chakra-ui/react';
 
 export default function Resume() {
-  useEffect(() => {
-    const anchor = document.createElement('a');
-    anchor.href = `https://drive.google.com/uc?export=download&id=1o2v7OyDkBc5XYZmSRGMcP-im6Ge7guD8`;
-
-    // Specify the filename for the downloaded file
-    anchor.download = 'Nimish_Shah_resume.pdf';
-
-    // Append the anchor to the body (necessary for Firefox)
-    document.body.appendChild(anchor);
-
-    // Programmatically trigger the click event
-    anchor.click();
-
-    // Remove the anchor from the document
-    document.body.removeChild(anchor);
-    window.location.href = '/';
-  }, []);
-  return <></>;
+  return (
+    <Container
+      display={'flex'}
+      flexDirection={'row'}
+      justifyContent={'center'}
+      alignContent={'center'}
+      minWidth={'100vw'}
+    >
+      <iframe
+        style={{
+          width: '40vw',
+          height: '100vh',
+        }}
+        src="https://drive.google.com/file/d/1o2v7OyDkBc5XYZmSRGMcP-im6Ge7guD8/preview"
+      />
+    </Container>
+  );
 }
